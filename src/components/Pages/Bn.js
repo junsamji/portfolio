04217a428623b5cd947cb4961bf0699300937a58
junsam.ji <cowtree84@gmail.com>
2021-07-29 16:52:36 +0900

    update

---
+++ src/components/Pages/Bn.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Bipex from '../../assets/Bn.png'
+
+function Bn() {
+    return(
+        <div>
+            <div><img src={Bipex}/></div>
+            <div>
+            <h1>주요 업무</h1>
+            </div>
+            <div>
+                <h2>- ERP개발 및 운영</h2>
+                <h2>- 그룹웨어 시스템 운영 (IBM Lotus Notes Solution)</h2>
+            </div>
+            <div>
+            <h1>프로젝트</h1>
+            </div>
+            <div>
+                <h2>- Cargo System</h2>
+                <h2>- CCM ( 골프장 통합관리 - 레이포드CC )</h2>
+                <h2>- 자산관리 시스템</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Bn;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,50 +1,73 @@
 import React from 'react';
 import {AnimatedOnScroll} from "react-animated-css-onscroll";
 
+import Bipex from './Bn';
 import './Home.css';
-
+import Bn from '../../assets/Bn.png';
+import Sejung from '../../assets/Sejung.png';
+import Hancom from '../../assets/hancom.png';
 
 function Home() {
     return (
         <div className="divC">
             <AnimatedOnScroll animationOut="fadeOut" animationInDuration={600}>
             <div className="homediv">
-                <h1> Home abcd abcd abcd abcd abcd abcd abcd</h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
+                <ul type="square">
+                    <li><h2>Name</h2>지준삼</li>
+                    <li><h2>Career</h2>since 2011.11.01</li>
+                    <li>
+                        <h2>Skill & Tools</h2>
+                        - Java, JavaScript, VisualBasic, C#<br/>
+                        - Spring, SpringBoot, .NET, react<br/>
+                        - Oracle, MSSQL<br/>
+                        - Docker, Jenkins
+                    </li>
+                </ul>
             </div>
             </AnimatedOnScroll>
             <AnimatedOnScroll animationIn="slideInUp" animationInDuration={1000}>
-            <div className="homediv">
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
+            <div className="homediv" style={{
+                    textAlign:'center',
+                    backgroundColor: '#ccc'}}>
+                <div style={{
+                    height:'10vh'
+                }}>
+                    <h1>경력사항</h1>
+                </div>
+                
+                <div style={{
+                    height:'25vh'
+                }}>
+                    <span><img src={Bn} height="100" alt="Bn" style={{
+                    border: '1px solid gold',
+                }}/></span>
+                    <span><h2>(주)바이펙스 (2011.11 ~ 2013.09)</h2></span>
+                </div>
+
+                <div style={{
+                    height:'25vh'
+                }}>
+                    <span><img src={Sejung} height="100" alt="Sejung" style={{
+                    border: '2px solid gold',
+                }}/></span>
+                    <span><h2>(주)세정아이앤씨 (2013.10 ~ 2019.08)</h2></span>
+                </div>
+
+                <div style={{
+                    height:'25vh'
+                }}>
+                    <span><img src={Hancom} height="100" alt="Hancom" style={{
+                    border: '2px solid gold',
+                }}/></span>
+                    <span><h2>(주)한글과컴퓨터 (2019.09 ~ )</h2></span>
+                </div>
             </div>
             </AnimatedOnScroll>
-            <AnimatedOnScroll animationIn="slideInUp" animationInDuration={1000}>
-            <div className="homediv">
-                <h1> Home </h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
-                <h1> Home </h1>
+            <div className="homediv" style={{
+                    textAlign:'center',
+                    backgroundColor: '#FFF136'}}>
+                <Bipex/>
             </div>
-            </AnimatedOnScroll>
-            <AnimatedOnScroll animationIn="slideInUp" animationInDuration={1000}>
-            <div className="homediv">
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-                <h2>test</h2>
-            </div>
-            </AnimatedOnScroll>
         </div>
     );
 };
