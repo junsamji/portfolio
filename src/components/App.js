0473050c782ee29f646d31527cae4bb2e1d0cac2
junsam.ji <cowtree84@gmail.com>
2021-07-29 14:12:14 +0900

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {AnimatedOnScroll} from "react-animated-css-onscroll";
 
